374fecc8056b3c631ac411de565d52512cf21c94
Rachel Fong <rachefo@gmail.com>
2013-02-27 01:53:17 -0500

    made non-editable node label show up

--- build/graph-editor.js
+++ build/graph-editor.js
@@ -1055,7 +1055,7 @@ function render_menu(div) {
     <div id='info'>Index: <span id='index'></span><br>\
     <span id='pos'>Position: (<span id='posx'></span>, <span id='posy'></span>)<br></span>\
     <span id='vert'>Vertices: <span id='v1'></span>-><span id='v2'></span><br></span>\
-    <span id='label-container'>Label: <input type='text' id='label'></span></div>\
+    <span id='label-container'>Label: <span id='label-plain'></span><input type='text' id='label'></span></div>\
     <div id='none_selected'>No node is selected</div></div>");
     $(div + ' .infobox #info').hide();
     $(div + ' .infobox #label').keyup(function(e) {
@@ -1116,7 +1116,7 @@ function render_menu(div) {
         MODIFIABLE_NODES = !MODIFIABLE_NODES;
         draw();
         if ($('.infobox #title').html() == 'Vertex Info')
-            update_infobox_label();
+            update_infobox_label(div);
         });
 
     $(menu).append('</table><table>');
@@ -1168,12 +1168,15 @@ function render_menu(div) {
     $(menu).append('<textarea id="json" rows="5" cols="34"></textarea><br>');
 }
 
-function update_infobox_label() {
-    console.log("UPDATE")
-    if (MODIFIABLE_NODES)
-        $('.infobox #label-container').show();
-    else
-        $('.infobox #label-container').hide();
+function update_infobox_label(div) {
+    if (MODIFIABLE_NODES) {
+        $(div + ' .infobox #label').show();
+        $(div + ' .infobox #label-plain').hide();
+    }
+    else {
+        $(div + ' .infobox #label').hide();
+        $(div + ' .infobox #label-plain').show();
+    }
 }
 
 function update_infobox(obj) {
@@ -1183,7 +1186,7 @@ function update_infobox(obj) {
     var pos, index, node, edge;
     if (obj && obj instanceof Vertex) {
         node = obj, pos = node.get_pos(), index = nodes.indexOf(node);
-        update_infobox_label();
+        update_infobox_label(div);
         $(div + ' .infobox #title').html('Vertex Info');
         $(div + ' .infobox #index').html(index);
         $(div + ' .infobox #pos').show();
@@ -1191,13 +1194,15 @@ function update_infobox(obj) {
         $(div + ' .infobox #posy').html(pos.y.toFixed(1));
         $(div + ' .infobox #vert').hide();
         $(div + ' .infobox #label').val(node.label);
+        $(div + ' .infobox #label-plain').html(node.label);
         $(div + ' .infobox #none_selected').hide();
         $(div + ' .infobox #info').show();
     } else if (obj && obj instanceof Edge) {
         edge = obj;
         var enodes = edge.get_nodes();
         index = edge_list.indexOf(edge);
-        $('.infobox #label-container').show();
+        $(div + ' .infobox #label').show();
+        $(div + ' .infobox #label-plain').hide();
         $(div + ' .infobox #title').html('Edge Info');
         $(div + ' .infobox #index').html(index);
         $(div + ' .infobox #pos').hide();
@@ -1207,6 +1212,7 @@ function update_infobox(obj) {
         $(div + ' .infobox #v1').html(enodes.node1.label);
         $(div + ' .infobox #v2').html(enodes.node2.label);
         $(div + ' .infobox #label').val(edge.label||'');
+        $(div + ' .infobox #label-plain').html(edge.label||'');
         $(div + ' .infobox #none_selected').hide();
         $(div + ' .infobox #info').show();
     } else {
